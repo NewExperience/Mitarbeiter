cde2c4989871152646f02496ff1efc7b6f00ffec
Andreas Gaul <agaul@Vostro-200.(none)>
2014-11-03 12:33:15 +0100

    new new test

--- data_add.php
+++ data_add.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno())
 {
  	echo "Failed to connect to MySQL: " .mysqli_connect_error();
 }
-
+//d
 $keys = array ("vorname", "name", "geburtsdatum", "geburtsort");
 $col = count($keys);
 
