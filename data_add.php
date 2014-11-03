e84649d75a30fdc62012cae5956c88870ec577ac
Andreas Gaul <agaul@Vostro-200.(none)>
2014-11-03 16:15:01 +0100

    Again original codes

--- data_add.php
+++ data_add.php
@@ -8,8 +8,6 @@ if (mysqli_connect_errno())
  	echo "Failed to connect to MySQL: " .mysqli_connect_error();
 }
 
-//Another stupid array!
-$keys = array ("Hallo", "ich", "bin", "Fede");
 $keys = array ("vorname", "name", "geburtsdatum", "geburtsort");
 $col = count($keys);
 
